a86f56445f20a4046ef9a83852a74c194c59ff86
mrosyidridlo <codeingrowbrain@gmail.com>
2019-02-27 11:17:42 +0700

    Updates

---
+++ precache-manifest.4f58a1b6367136fb62c6817e1dc0e554.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "7250a19556b2d947dec7",
+    "url": "/traveloka/static/css/main.56255dd5.chunk.css"
+  },
+  {
+    "revision": "7250a19556b2d947dec7",
+    "url": "/traveloka/static/js/main.7250a195.chunk.js"
+  },
+  {
+    "revision": "f39d7901bab85bacadea",
+    "url": "/traveloka/static/css/1.860391b3.chunk.css"
+  },
+  {
+    "revision": "f39d7901bab85bacadea",
+    "url": "/traveloka/static/js/1.f39d7901.chunk.js"
+  },
+  {
+    "revision": "e1dc65a5518aab2d2b16",
+    "url": "/traveloka/static/js/runtime~main.e1dc65a5.js"
+  },
+  {
+    "revision": "82e21c3691fd4666d6584b47358641c4",
+    "url": "/traveloka/static/media/rate.82e21c36.svg"
+  },
+  {
+    "revision": "cdd24f1ea578e8f2c458991cc9b8af7a",
+    "url": "/traveloka/static/media/tanggal.cdd24f1e.svg"
+  },
+  {
+    "revision": "9964250a428f298ee1563748de284f53",
+    "url": "/traveloka/static/media/traveloka.9964250a.svg"
+  },
+  {
+    "revision": "f8a952073a5d3f5cd78c3042a9944dc2",
+    "url": "/traveloka/static/media/24.f8a95207.svg"
+  },
+  {
+    "revision": "126daeb4ff5baa9ed5a27262ba913ecb",
+    "url": "/traveloka/static/media/logo1.126daeb4.svg"
+  },
+  {
+    "revision": "031847b21d9c086a4905edda1dbed54f",
+    "url": "/traveloka/static/media/logo2.031847b2.svg"
+  },
+  {
+    "revision": "7108e70e0af8466638e133e7401cc740",
+    "url": "/traveloka/static/media/logo3.7108e70e.svg"
+  },
+  {
+    "revision": "4f13b9964acc7db26109edce76936eb4",
+    "url": "/traveloka/static/media/googleplay.4f13b996.svg"
+  },
+  {
+    "revision": "0e4d33af6775eb7f5d469ebd806909a3",
+    "url": "/traveloka/static/media/appstore.0e4d33af.svg"
+  },
+  {
+    "revision": "d3063bc99f0978a11b088cc8b3656f14",
+    "url": "/traveloka/static/media/logo.d3063bc9.svg"
+  },
+  {
+    "revision": "8f3194fe1f8b18b90c757cee44b613b0",
+    "url": "/traveloka/static/media/burger.8f3194fe.svg"
+  },
+  {
+    "revision": "52d53a870dc8085706bbe27c26ca6407",
+    "url": "/traveloka/static/media/LastView.52d53a87.svg"
+  },
+  {
+    "revision": "078a2898f890bf4124cd4bdb60a7a0f9",
+    "url": "/traveloka/static/media/Location.078a2898.svg"
+  },
+  {
+    "revision": "a61e8e3f410175a4c89aa5a54200175c",
+    "url": "/traveloka/static/media/Payment.a61e8e3f.svg"
+  },
+  {
+    "revision": "8fa0ebf1d3beacc68fc9ff2eae21b690",
+    "url": "/traveloka/static/media/BestPrice.8fa0ebf1.svg"
+  },
+  {
+    "revision": "79030054dbdd544cc255fc85200e00c7",
+    "url": "/traveloka/static/media/Search.79030054.svg"
+  },
+  {
+    "revision": "e2455e19650a0915fbe285d6b10bb69f",
+    "url": "/traveloka/static/media/Flight.e2455e19.svg"
+  },
+  {
+    "revision": "7d3b8cf2c7ec938d3a7eb8320e04af31",
+    "url": "/traveloka/static/media/Hotel.7d3b8cf2.svg"
+  },
+  {
+    "revision": "aa93d08c8a7ca22941e70a94b3521f68",
+    "url": "/traveloka/static/media/Train.aa93d08c.svg"
+  },
+  {
+    "revision": "3e2f1a5d813403bff0b78235ff69f1d7",
+    "url": "/traveloka/static/media/BusShutle.3e2f1a5d.svg"
+  },
+  {
+    "revision": "61b631eb7ee69ac942adbb31b373176b",
+    "url": "/traveloka/static/media/Airport.61b631eb.svg"
+  },
+  {
+    "revision": "fc32ad057e5a830a8ec0f0cc9281a80e",
+    "url": "/traveloka/static/media/FlightHotel.fc32ad05.svg"
+  },
+  {
+    "revision": "28178074f8166c251ca2050e428b7887",
+    "url": "/traveloka/static/media/Attractions.28178074.svg"
+  },
+  {
+    "revision": "cea21463547837660c1ad5d02662809c",
+    "url": "/traveloka/static/media/Topup.cea21463.svg"
+  },
+  {
+    "revision": "17bf82d76a59392bbf4156e19cc3033e",
+    "url": "/traveloka/static/media/lokasi.17bf82d7.svg"
+  },
+  {
+    "revision": "b7242ee55442a7ed93214dfd3561968e",
+    "url": "/traveloka/static/media/bandara.b7242ee5.svg"
+  },
+  {
+    "revision": "a300439d546977ab57334dcfce0cdd9a",
+    "url": "/traveloka/static/media/hotel.a300439d.svg"
+  },
+  {
+    "revision": "e9388716fc3437d3d6473cb9e53ed4ec",
+    "url": "/traveloka/static/media/tagihan.e9388716.svg"
+  },
+  {
+    "revision": "068f8b76d0dee478620215e002da5305",
+    "url": "/traveloka/static/media/wifi.068f8b76.svg"
+  },
+  {
+    "revision": "9390cfe5f9e07003e7c342cda10cc7c9",
+    "url": "/traveloka/static/media/check-icon.9390cfe5.png"
+  },
+  {
+    "revision": "452ac5e6b678b9999c5da1fd5245ff5c",
+    "url": "/traveloka/static/media/Hotel_Airport_Jakarta.452ac5e6.png"
+  },
+  {
+    "revision": "03cc0f5c3245428c54ffe08f5cc27284",
+    "url": "/traveloka/index.html"
+  }
+];
