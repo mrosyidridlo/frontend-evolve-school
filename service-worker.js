0b5295a047bccf885f3f9d03035edb793a118198
mrosyidridlo <codeingrowbrain@gmail.com>
2019-04-09 08:10:40 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/traveloka/precache-manifest.4f58a1b6367136fb62c6817e1dc0e554.js"
+  "/frontrnd-final-project/precache-manifest.07996f3e73cbf53b00c496b926910bed.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/traveloka/index.html", {
+workbox.routing.registerNavigationRoute("/frontrnd-final-project/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
