b00590f14b900b82c8b27d64bca3d601ac005fcf
mrosyidridlo <codeingrowbrain@gmail.com>
2019-04-09 23:10:07 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/traveloka/precache-manifest.4f58a1b6367136fb62c6817e1dc0e554.js"
+  "/frontend-evolve-school/precache-manifest.1bd79b7b8ed2d97c7c5f859776497e88.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/traveloka/index.html", {
+workbox.routing.registerNavigationRoute("/frontend-evolve-school/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
